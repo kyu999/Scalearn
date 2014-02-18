f383e3ef13c25d3dae37d73d93f44e8969d6e79a
kyu999 <b1100574@gl.aiu.ac.jp>
2014-02-18 19:06:29 +0700

    ちょちょっと修正。

--- src/datastore/Dataset.scala
+++ src/datastore/Dataset.scala
@@ -1,16 +1,16 @@
 package datastore
 
-//import scala.collection.parallel.immutable.ParSeq
 class Dataset(x:Stream[Double],y:Stream[Double]){
-//	def read(newvar:Stream[Double])={x=newvar}
+
 	def ave(data:Stream[Double]):Double=data.reduce((a,b)=>a+b)/data.length
 	//平均
 	def dev(data:Stream[Double],average:Double):Stream[Double]=data.map(x=>x-average)
 	//各偏差
-
+	
 	def devto2(devdata:Stream[Double]):Stream[Double]=devdata.map(x=>(math.pow(x,2))) 
 	//各偏差の２乗
-	def sd(dv2:Stream[Double])=math.sqrt(dv2.reduce((a,b)=>a+b)/(dv2.length-1))	
+	//偏差の２乗して平方根とるんじゃなくて偏差の絶対値をとったほうが効率良いかな？
+	def sd(dv2:Stream[Double]):Double=math.sqrt(dv2.reduce((a,b)=>a+b)/(dv2.length-1))	
 	//√分散＝標準偏差
 	def zipdev(dev1:Stream[Double],dev2:Stream[Double]):Stream[(Double,Double)]=dev1.zip(dev2)
 	//2つのデータセットの偏差をTuple化
