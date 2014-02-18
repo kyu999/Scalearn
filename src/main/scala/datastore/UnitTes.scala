0b990d4018b011e35d74edd87c8c00d85b658ef6
kyu999 <b1100574@gl.aiu.ac.jp>
2014-02-18 23:23:19 +0700

    slick&sbt導入
    sbtは復習要。
    sbtとEclipseの統合をsbtのEclipseプラグインで行ったが完璧ではなくsbtではエラーがでないのにEclipseではエラーなどが見
    られた。slick等ライブラリを使う場合はコンパイルなどsbtで行う必要がありrunもsbtからの方がいいかも。

---
+++ src/main/scala/datastore/UnitTes.scala
@@ -0,0 +1,10 @@
+package datastore
+
+object UnitTes extends App{
+	val x=Stream(0.5,3,5,3,3,6,7,8,5,2,9) 
+	val y=Stream(5.0,3,5,3,3,6,7,8,5,2) 
+	val data=new Dataset(x,y)
+	data.summary
+	println(data.xdev1.length)
+	println(data.zipXY.length)
+}
