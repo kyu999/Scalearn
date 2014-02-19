76c7d2ff4cc225ca7b3247fd1f3ab0c7e3d45708
kyu999 <b1100574@gl.aiu.ac.jp>
2014-02-19 16:01:42 +0700

    ごちゃごちゃしてたから仮想データベース的なノリ

--- src/main/scala/classifier/NaiveBayes.scala
+++ src/main/scala/classifier/NaiveBayes.scala
@@ -8,29 +8,20 @@ import tokenfactory.kuro
 object Tes extends App{
   val t=NaiveBayes.time _
   val f=NaiveBayes.examine _  //オブジェクトのメソッドは関数ではないので関数化するには　メソッド名+ _　 
-  t(f,"失敗成功")
-  println("")
-  t(f,"彼は私に気があるのでしょうか？") 
-  println("")
-  t(f,"""
-もしうまくいっても、４月から私は東京に戻ってしまいます。
-それに教習所の先生はもてますよね…。
-私は今まで誰かと付き合ったことがないのでよくわかりません。
-これは、やめておいた方がいい恋なのでしょうか？""")
- 
+  VirtualData.examlist.map(x=>f(x))
+  
 }
 object NaiveBayes{	
   //並列コレクション化した方が効率上がるかもparメソッドの活用=>並列化によるオーバーヘッドが大きいため現状では使わないほうがいい。
   var data:ListBuffer[(Boolean,String)]
-		  =ListBuffer((true,"成功"),(false,"失敗"))
-		  //元の値達は学習データ。data,Sdata,Fdataはmutable。falseの学習データには失敗しか入ってないけどtrueの方に可愛と成功が入ってる。これによって失敗という単語の重みは成功や可愛という単語よりも大きくなる。故に可愛いと失敗が１語づつのStringを調査するときは失敗に傾く。
+		  =VirtualData.datalist
+  //元の値達は学習データ。data,Sdata,Fdataはmutable。falseの学習データには失敗しか入ってないけどtrueの方に可愛と成功が入ってる。これによって失敗という単語の重みは成功や可愛という単語よりも大きくなる。故に可愛いと失敗が１語づつのStringを調査するときは失敗に傾く。
     
   var Sdata=data.filter(x=>x._1==true)  //成功data。mutable
   var Fdata=data.filter(x=>x._1==false) //失敗data。mutable
   
   def preS:Double=log((Sdata.length-1).toDouble/data.length)	//examineでSdataには最初に調べる項目が追加されるがdata自体には関数の最後に追加される。それによって成功事例がおおく見積もられるのを防ぐためにSdataに-1をしてる
   def preF=log((Fdata.length-1).toDouble/data.length)
-    //log(1-preS)	
   //成功、失敗それぞれの事前確率。examineの中で使う。ただし追加したpostが影響しないようにvalを使おうと思ったけどdata数とSdata数との関係が崩れるからやめた
   
   def getString(alldata:ListBuffer[(Boolean,String)])=alldata.map(x=>x._2).mkString
@@ -53,7 +44,7 @@ object NaiveBayes{
   def Swp=WordPro(Swords,numTokenS)
   def Fwp=WordPro(Fwords,numTokenF)
   
-  def allyudo(proList:List[Double]):Double=proList.reduce((a,b)=>{println("a : "+a+", b : "+b);a+b}) //本来a*bだがlogの計算なのでa+b；log(a*b)=log(a)+log(b)
+  def allyudo(proList:List[Double]):Double=proList.reduce((a,b)=>a+b) //本来a*bだがlogの計算なのでa+b；log(a*b)=log(a)+log(b)
     
   def time(f:String=>Unit,post:String)={
     val start=System.currentTimeMillis
@@ -76,9 +67,7 @@ object NaiveBayes{
     val yudoSList=dic.map(x=>Swp(x))
     val yudoFList=dic.map(x=>Fwp(x))
     
-    println("SaftLog start")
     val Saft=allyudo(yudoSList)*preS
-    println("FaftLog start")
     val Faft=allyudo(yudoFList)*preF
  
     summary
