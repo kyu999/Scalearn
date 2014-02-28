4f0f6cc5e75c8462c918e615365b7ffc341c64a3
kyu999 <b1100574@gl.aiu.ac.jp>
2014-02-28 13:21:44 +0700

    正規分布の確率密度関数実装

--- src/main/scala/datafactory/Inference.scala
+++ src/main/scala/datafactory/Inference.scala
@@ -1,6 +1,10 @@
 package datafactory
-
+import scala.math._
 trait Inference extends Descritive{
-//t examine
+	def paired_t_test(rawX:Seq[Double],rawY:Seq[Double]){
+	  val gap=rawX.zip(rawY).map(a=>a._1-a._2)
+	  val t=meanf(gap)/(stdevi(gap)/sqrt(gap.length))
+	  //t分布の確率密度関数からp値を取得。。。ただし自由度が絡んでいるので分布表をMapや関数化した方がてっとりばやいかも
+	}
 //f examine
 }
---
+++ src/main/scala/distribution/normal.scala
@@ -0,0 +1,24 @@
+package distribution
+import scala.math._
+class normal(mu:Double,sigma:Double) {
+	def pdf:Double=>Double={
+	val f={
+	  (x:Double)=>
+	val logged=log(1/sqrt(2*Pi*pow(sigma,2))) - ( pow(x-mu,2) )/(2*pow(sigma,2))
+	println("logged : "+logged)
+	val ans=pow(E,logged)
+	println(ans)
+	ans
+	}
+	f
+	}
+	//確率密度関数
+}
+object testes extends App{
+  val n=new normal(100,5)
+  println(n.pdf(102))
+  println(n.pdf(96))
+  val x:BigDecimal=10
+}
+
+//桁数漏れが起こる。=>対数とる？
