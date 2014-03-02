34425657302901c43fe49cb0aa956f0aed645cc4
kyu999 <b1100574@gl.aiu.ac.jp>
2014-03-02 12:56:36 +0700

    ガンマ関数実装完了。stirlingの近似は念のため100以上で用いる。それ以下はfactorialで。

--- src/main/scala/distribution/gamma.scala
+++ src/main/scala/distribution/gamma.scala
@@ -1,35 +1,33 @@
 package distribution
 import scala.math._
 object gamma {
-	def pdf(n:Int):Double={
-	  val logged=n*(log(n)-1)+log(2*Pi*n)/2
-	  println("gammma pdf logged : "+logged)
+	
+	def stirling(n:Int):Double={
+	  val logged=n*(log(n)-1)+(log(2*Pi*n))/2
 	  logged
 	}
-	def cul(n:Int):Double={
-	  val logged=n*(log(n)-1)+(log(2*Pi*n))/2
-	  println("gamma value logged : "+logged)
-	  pow(E,logged)
-	}
+	//n>100くらいから近似でstirlingが使えるかな。n<100まではfactorialで。
+	
 	def factorial(n:Double):Double={
 	  if(n<=1) log(1)
 	  else log(n)+factorial(n-1)
 	}
-	//桁数漏れ対策でlogで計算
+	//桁数漏れ対策でlogで計算.
 	
-	//cul関数は正しくない。ログの値が４つほどずれている。＝３桁前後ずれている
 	/*     
 	ガンマ関数の計算にはlengthが小さいなら（自由度が小さいなら）通常のfactorial関数で。
     nが大きく計算にかかる負荷が大きくなりそうならスターリングの近似式を活用。
     log(n!)=>n(log(n)-1)+(1/2)*log(2*Pi*n)
-    n!=>√(2*Pi*n)*pow(n,n)*pow(E,-n)
 	 
 	 powでlogから直す段階で桁数溢れが勃発。logのままで行った方が良いかも
 	 */
 }
 
 object gammates extends App{
-  println(gamma.cul(50))
-  (4 to 7).map(a=>gamma.factorial(a)).foreach(println)
-  println(gamma.factorial(1000))
+  println(gamma.stirling(50))
+  println("factorial")
+  (0 to 10).map(a=>pow(E,gamma.factorial(a))).foreach(println)
+  println("stirling")
+  (0 to 10).map(a=>pow(E,gamma.stirling(a))).foreach(println)
+  println("correct : "+pow(E,363.7)+" , stirling : "+pow(E,gamma.stirling(100))+" , factorial : "+pow(E,gamma.factorial(100)))
 }
