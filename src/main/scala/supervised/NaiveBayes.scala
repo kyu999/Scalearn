783c1ac9d507eac6dace7922643e6833e1972def
kyu999 <b1100574@gl.aiu.ac.jp>
2014-04-08 22:35:53 +0700

    allow mutable state in file_paths in NB on memory

--- src/main/scala/supervised/NaiveBayes.scala
+++ src/main/scala/supervised/NaiveBayes.scala
@@ -3,26 +3,29 @@ package supervised
 import org.apache.spark.rdd
 import scala.math.log
 import io.read
+import scala.collection.mutable.ListBuffer
 
 /**
 input : Vector[ (the class name the document belong to , path to document ) ]
+only the file_paths is mutable variable, the other things are just functions
 **/
 
-case class NaiveBayes( file_pathes : Vector[(String,String)] )
+
+case class NaiveBayes( var file_paths : ListBuffer[(String,String)] )
 {
 	
-	val docs:Vector[(String ,Array[(String,Int)])] = file_pathes.map( class_path => ( class_path._1 , read.rdds(class_path._2,false).collect ) )
+	def docs:List[(String ,Array[(String,Int)])] = file_paths.map( class_path => ( class_path._1 , read.rdds(class_path._2,false).collect ) ).toList
 	
-	val wholeClass :Map[String,Vector[(String,Array[(String,Int)])]] = docs.groupBy(elt=>elt._1)
-	//全てのクラスとドキュメントの集合：Map( 各クラス-> Vector( ( 各クラス,ドキュメントの集合 ) ) )
+	def wholeClass :Map[String,List[(String,Array[(String,Int)])]] = docs.groupBy(elt=>elt._1)
+	//全てのクラスとドキュメントの集合：Map( 各クラス-> List( ( 各クラス,ドキュメントの集合 ) ) )
 	
-	val allClassNames:Vector[String] = wholeClass.map(elt=>elt._1).toVector
+	def allClassNames:List[String] = wholeClass.map(elt=>elt._1).toList
 	//全classのリスト作成
 	
-	val eachNumDocs:Map[String,Int] = wholeClass.map(elt=>(elt._1,elt._2.length))
+	def eachNumDocs:Map[String,Int] = wholeClass.map(elt=>(elt._1,elt._2.length))
 	//Nc：各クラスに置けるdocument数、のMap : Map(class->number of docs)
 	
-	val sumNumDocs:Int = docs.size
+	def sumNumDocs:Int = docs.size
 	//ΣNc：総document数
 	
 	def eachNumWord(word:String , class_name:String ):Int = {
@@ -40,7 +43,7 @@ case class NaiveBayes( file_pathes : Vector[(String,String)] )
 	}
 	//N(w,c)：各クラスに置ける各単語の出現回数、のMap。できれば。もし未知語が来たらNoneではなく0を返す
 	
-	def eachProbWord(word:String , class_name:String , alpha:Int = 2):Double={
+	def eachProbWord(word:String , class_name:String , alpha:Int = 2):Double = {
 		
 		val Nwc = eachNumWord(word , class_name).toDouble
 		val Nc = eachNumDocs(class_name).toDouble
@@ -50,7 +53,7 @@ case class NaiveBayes( file_pathes : Vector[(String,String)] )
 	//log(Pw,c)
 	//alpha is the parameter to decide how much we gonna make the data flat
 	
-	def eachProbClass(class_name:String):Double={
+	def eachProbClass(class_name:String):Double = {
 		
 		val Nc = eachNumDocs(class_name).toDouble
 		
@@ -58,11 +61,11 @@ case class NaiveBayes( file_pathes : Vector[(String,String)] )
 
 	}
 	
-	val NumClass = wholeClass.size
+	def NumClass = wholeClass.size
 	//|C|：クラスの種類数
 	
 
-	def classify(doc_path:String , alpha:Int = 2 ) = {
+	def classify(doc_path:String , alpha:Int = 2 ):String = {
 
 		val arrayWord = read.rdds(doc_path).collect	//何度も使うのでcache化
 								
@@ -79,10 +82,14 @@ case class NaiveBayes( file_pathes : Vector[(String,String)] )
 		
         println("ProbPerClass : "+ProbPerClass.zip(allClassNames))
         
-		val max_class : (Double,Int) = ProbPerClass.zipWithIndex.max
+		val max_prob_index : (Double,Int) = ProbPerClass.zipWithIndex.max
 		// ( probability , index of the class )
 		
-		allClassNames(max_class._2)
+		val estimate_class:String = allClassNames(max_prob_index._2)
+        
+        file_paths += Pair(estimate_class,doc_path)
+        
+        estimate_class
 		//推定クラスを返す
 		
 		
@@ -100,17 +107,23 @@ case class NaiveBayes( file_pathes : Vector[(String,String)] )
 
 object DoNaiveBayes extends App{
 
-  	val file_pathes = 
-  		Vector( ("plus","resource/doc1.txt"),
+  	val file_paths = 
+  		ListBuffer( ("plus","resource/doc1.txt"),
   			    ("plus","resource/doc2.txt"),
   			    ("plus","resource/doc3.txt"),
   			    ("minus","resource/doc4.txt"),
   			    ("minus","resource/doc5.txt"),
   			    ("minus","resource/doc6.txt")
   			  )
-  	
-  	val pn = NaiveBayes(file_pathes)  	
+  	      
+  	val pn = NaiveBayes(file_paths)  	
 
- 	println("classify : "+ pn.classify("resource/examine.txt") ) 	
+    println("previous file_paths : " + pn.file_paths)  
+
+ 	println("classify : " + pn.classify("resource/examine.txt") ) 
+     
+    println("after file_paths : " + pn.file_paths)
+     
+    
  	     
 }
