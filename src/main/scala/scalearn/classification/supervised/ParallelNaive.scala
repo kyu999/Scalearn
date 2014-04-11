ce251f35e46ceff330638a246d95c516736c48f4
kyu999 <b1100574@gl.aiu.ac.jp>
2014-04-11 21:36:02 +0700

    arrange appearance of code

--- src/main/scala/scalearn/classification/supervised/ParallelNaive.scala
+++ src/main/scala/scalearn/classification/supervised/ParallelNaive.scala
@@ -13,112 +13,90 @@ import org.apache.spark.SparkContext
 
 /**
 input : ListBuffer[ (the class name the document belong to , path to document ) ]
-file_paths & docs are mutable !! Be careful. it has side effect.
+file_paths & docs are mutable !! Be careful. they are subject to side effect.
 **/
 
 case class ParallelNaive( 
-	file_paths : ListBuffer[(String,String)] , 
-	spark_context:SparkContext = SparkInstance.default) extends Serializable 
+	file_paths :ListBuffer[(String,String)] , 
+	spark_context :SparkContext = SparkInstance.default ) extends Serializable 
 {
 	
-	val docs:ListBuffer[(String ,RDD[(String,Int)])] = file_paths.map( class_path => ( class_path._1 , read.rdds(class_path._2,false,spark_context) ) )
+	val docs :ListBuffer[(String ,RDD[(String,Int)])] = 
+        file_paths.map( 
+            class_path => 
+                ( class_path._1 , read.rdds(class_path._2,false,spark_context) ) 
+            )
 	
 	def wholeClass :Map[String,ListBuffer[(String,RDD[(String,Int)])]] = docs.groupBy(elt=>elt._1)
 	//全てのクラスとドキュメントの集合：Map( 各クラス-> Vector( ( 各クラス,ドキュメントの集合 ) ) )
 	
-	def allClassNames:List[String] = wholeClass.map(elt=>elt._1).toList
+	def allClassNames :List[String] = wholeClass.map(elt=>elt._1).toList
 	//全classのリスト作成
 	
-	def eachNumDocs:Map[String,Int] = wholeClass.map(elt=>(elt._1,elt._2.length))
+	def eachNumDocs :Map[String,Int] = wholeClass.map(elt=>(elt._1,elt._2.length))
 	//Nc：各クラスに置けるdocument数、のMap : Map(class->number of docs)
 	
-	def sumNumDocs:Int = docs.size
+	def sumNumDocs :Int = docs.size
 	//ΣNc：総document数
 	
 	def eachNumWord(word:String , class_name:String ):Int = {
-
-		println(" eachNumWord---------------------------------------------")
 				
 		var doc_count = 0
 
-		wholeClass(class_name).foreach{
-			class_rdd =>  // == (class,rdd)
+		wholeClass(class_name).foreach{ 
+            class_rdd =>  // == (class,rdd)
 				val filtered = class_rdd._2.filter{word_occur=> word_occur._1==word}
 				
 				if(filtered.count != 0) doc_count += 1
-		}
+		    }
 		
-		println("eachNumWord Done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 		doc_count
 	}
 	//N(w,c)：各クラスに置ける各単語の出現回数、のMap。できれば。もし未知語が来たらNoneではなく0を返す
 	
 	def eachProbWord(word:String , class_name:String , alpha:Int = 2):Double={
-		println(" eachProbWord---------------------------------------------")
 		
 		val Nwc = eachNumWord(word , class_name).toDouble
 		val Nc = eachNumDocs(class_name).toDouble
-		
-		println("Nwc : "+Nwc)
-		println("Nc : "+Nc)
-		
+				
 		log( ( Nwc+(alpha-1) ) / ( Nc + 2*(alpha-1) ) )
 	}
 	//log(Pw,c)
 	//alpha is the parameter to decide how much we gonna make the data flat
 	
 	def eachProbClass(class_name:String):Double={
-		println(" eachProbClass---------------------------------------------")
 		
 		val Nc = eachNumDocs(class_name).toDouble
 		
-		log( ( Nc+1 ) / ( sumNumDocs + NumClass ) )
+		log( ( Nc+1 ) / ( sumNumDocs + numClass ) )
 
 	}
 	
-	val NumClass = wholeClass.size
+	val numClass :Int = wholeClass.size
 	//|C|：クラスの種類数
-	
-	def temp(doc_path:String) ={
-	
-		val cached_rdd = read.rdds(doc_path)
-		
-		println ( "cached_rdd : "+cached_rdd.collect() ) 	
-		println ( "cached_rdd : "+cached_rdd.collect() ) 	//okならactionの回数は問題ではない
-		
-		val each_prob : List[RDD[Double]] =
-			allClassNames.map{
-				class_name => 	
-						cached_rdd
-							.map { elt => eachProbWord(elt._1 , class_name ) * elt._2 }
-			}
-					
-		each_prob.map(_.take(1))
 
-//					val sum_prob  = each_prob.reduce{ (a,b) => a+b } 
-			
-	}
-	
-
-	def classify(doc_path:String , alpha:Int = 2 ) = {
+	def classify(doc_path:String , alpha:Int = 2 ):(Double,String) = {
 
         val new_rdd = read.rdds(doc_path)
         
-		val array_word_freq:Array[(String,Int)] = new_rdd.collect	
+		val array_word_freq :Array[(String,Int)] = new_rdd.collect	
 								
-		val ProbPerClass:List[(Double,String)] = 
-			allClassNames.map{
-				class_name => 					
-										
-					val each_prob :Array[Double] = 
-						array_word_freq.map { word_freq => eachProbWord(word_freq._1 , class_name , alpha) * word_freq._2 }
+		val probPerClass :List[(Double,String)] = 
+            allClassNames.map{   
+                class_name => 															
+                    
+                    val each_prob :Array[Double] = 
+					    array_word_freq.map {   
+                            word_freq => 
+                                eachProbWord(word_freq._1 , class_name , alpha) * word_freq._2 
+                            }
 															
-					( each_prob.sum + eachProbClass(class_name) , class_name )
-				}	
+				    ( each_prob.sum + eachProbClass(class_name) , class_name )
+			    }	
 
-		println("ProbPerClass : "+ProbPerClass)
+		println("probability of each class : " + probPerClass)
 		
-		val estimate_class : (Double,String) = ProbPerClass.max
+		val estimate_class :(Double,String) = probPerClass.max
 		
         file_paths += Pair(estimate_class._2,doc_path)
         
