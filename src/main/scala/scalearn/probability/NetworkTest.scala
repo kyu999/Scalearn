9918d043adc5448e93d0a3b205c86a508eb48f1e
kyu999 <kyukokkyou999@gmail.com>
2014-04-25 23:47:29 +0900

    Edge主体ではなくNode主体の実装のほうがMemory的にいいかも

--- src/main/scala/scalearn/probability/NetworkTest.scala
+++ src/main/scala/scalearn/probability/NetworkTest.scala
@@ -1,6 +1,36 @@
 package probability
     
+import scala.collection.mutable.Set
+        
 object NetworkTest extends App{
     
+    val event1 = Event("get A",0.3)
+    val event2 = Event("graduate successfully",0.2)
+    val event3 = Event("Bad score for SAT",0.1)
+        
+    val eventsA = Vector(event1,event2,event3)
+        
+    val player1 = Player(eventsA)
+        
+    player1.cpd.foreach(println)
+
+    val event4 = Event("get C",0.2)
+    val event5 = Event("fail to graduate",0.4)
+    val event6 = Event("best score ever!!",0.3)
+        
+    val eventsB = Vector(event4,event5,event6)
+      
+    val player2 = Player(eventsB)
     
+    println("")
+        
+    player2.cpd.foreach(println)
+            
+    val relation1 = DirectedRelation(player1,player2)
+        
+    val bn = BayesianNetwork(Set(relation1)) 
+    
+    println("BN : "+bn)
+            
+    bn.relations.foreach(println)
 }
--- src/main/scala/scalearn/probability/Probability.scala
+++ src/main/scala/scalearn/probability/Probability.scala
@@ -1,24 +1,30 @@
 package probability
-    
-package object probability{
-    
-import scala.collection.mutable.Map
-    
-    type Situation = String
-     
-    type Name = String
-       
         
-    case class Event(situation:Situation, probability:Double)
+import scala.collection.mutable.Set
+       
+    
+    case class Event(situation:String, probability:Double)
 
-    case class Player(events:Vector[Event], children:Vector[Player]){
+    case class Player(events:Vector[Event]){
 
+        val cpd : Vector[(String,Double)] = 
+            events
+                .combinations(2)
+                .map{ 
+                    eventPair => 
+                        ( eventPair(0).situation + " && " + eventPair(1).situation , 
+                        eventPair(0).probability * eventPair(1).probability )
+                }
+                .toVector
+        //cpd == Conditional Probability Distribution ; event同士は独立であると仮定する
+ 
         def indepenentWith(player:Player) = { }
         //player同士の関係性をデータから得る関数も実装する必要あり
+    
     }
 
 
-    trait Edge{
+    trait Relation{
     
         val start : Player
     
@@ -26,23 +32,24 @@ import scala.collection.mutable.Map
         
     }
 
-    case class DirectedEdge(start:Player,end:Player,toChild:Boolean) extends Edge
+    case class DirectedRelation(start:Player,end:Player) extends Relation
 
-    case class UndirectedEdge(start:Player,end:Player) extends Edge
+    case class UndirectedRelation(start:Player,end:Player) extends Relation
 
         
     trait Network {
             
-        val players : Map[Name,Player]
+        val relations : Set[Relation]
+        //mutable Set
             
-        def addPlayer(name:Name, player:Player) = 
-            players +=  name -> player
-
+        def addRelation(relation:Relation) = 
+            relations += relation
+        //side effect 
+            
     }
 
-    case class BayesianNetwork(players:Map[Name,Player]) extends Network
+    case class BayesianNetwork(relations:Set[Relation]) extends Network
 
-    case class MarkovNetwork(players:Map[Name,Player]) extends Network
+    case class MarkovNetwork(relations:Set[Relation]) extends Network
 
         
-}
