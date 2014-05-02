451442c66a48f663ffecf97a7b496a4ff2508cfc
kyu999 <kyukokkyou999@gmail.com>
2014-05-02 11:06:42 +0900

    Update LinearRegression.scala

--- src/main/scala/scalearn/classification/supervised/LinearRegression.scala
+++ src/main/scala/scalearn/classification/supervised/LinearRegression.scala
@@ -29,13 +29,13 @@ trait LinearRegression extends Descriptive{
 	  while (check){
 	    
 	    tempTheta0 = 
-            theta0 - alpha * (zipped.map(a=>theta0+theta1*a._1-a._2).sum)/size
+            	theta0 - alpha * (zipped.map(a=>theta0+theta1*a._1-a._2).sum)/size
 	
-        tempTheta1 = 
-            theta1 - alpha * (zipped.map(a=>a._1*(theta0+theta1*a._1-a._2)).sum)/size
+        　　tempTheta1 = 
+            	theta1 - alpha * (zipped.map(a=>a._1*(theta0+theta1*a._1-a._2)).sum)/size
 	    
 	    if( abs(theta0-tempTheta0) < alpha*0.01 && abs(theta1-tempTheta1) < alpha*0.01)
-            check = false
+            	check = false
 	    
 	    theta0 = tempTheta0
 	    theta1 = tempTheta1
@@ -45,7 +45,7 @@ trait LinearRegression extends Descriptive{
             + " , tempTheta1 : " + tempTheta1 
             + " ,  Theta0 : " + theta0
             + " , Theta1 : "+theta1
-        )
+            )
 	    
 	    counter = counter+1
 	    
