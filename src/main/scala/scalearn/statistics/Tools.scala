89637777d8300c0fed88b5bba0ff1f80820dd2b5
kyu999 <kyukokkyou999@gmail.com>
2014-05-03 15:27:56 +0900

    Update Tools.scala

--- src/main/scala/scalearn/statistics/Tools.scala
+++ src/main/scala/scalearn/statistics/Tools.scala
@@ -1,10 +1,10 @@
 package scalearn.statistics
 
 object Tools
-		extends Descriptive 
-		with Inference 
-		with TimeSeries 
-		with Bayes 
-		with Multivariate
+    extends Descriptive 
+    with Inference 
+    with TimeSeries 
+    with Bayes 
+    with Multivariate
 			
 //全く関係ない場所で様々な統計の関数を使いたい場合にこのオブジェクトをimportしよう
