af0af705bb81f23ffdbf832ffe5ac03e50cec628
kyu999 <kyukokkyou999@gmail.com>
2014-05-03 14:13:19 +0900

    Update JP.scala

--- src/main/scala/scalearn/preprocessing/tokenfactory/JP.scala
+++ src/main/scala/scalearn/preprocessing/tokenfactory/JP.scala
@@ -5,59 +5,59 @@ import scala.collection.convert.WrapAsScala._
 import scala.io.Source
 
 object  JP{
-	
-  	def mkToken(content:String):Seq[Token]= 
+
+    def mkToken(content:String):Seq[Token] = 
         Tokenizer
             .builder()
             .build()
             .tokenize(content)
             .filter( (x:Token) => x.getBaseForm != null )
 
-	def mkTokenF(path:String):Stream[Token] = {
-        val file=Source.fromFile(path)
-        val content=file.getLines.mkString
+    def mkTokenF(path:String):Stream[Token] = {
+        val file = Source.fromFile(path)
+        val content = file.getLines.mkString
         mkToken(content).toStream
-	}
+        }
 	//File pathからToken作成
 	
-	def nonrepeatW(stream:Stream[Token]):Stream[String] = {
-	  stream
-	      .groupBy(x=>x.getBaseForm)
-	      .values
-	      .toStream
-	      .map(x=>x(0).getBaseForm)
+    def nonrepeatW(stream:Stream[Token]):Stream[String] = {
+        stream
+            .groupBy(x=>x.getBaseForm)
+            .values
+            .toStream
+            .map(x=>x(0).getBaseForm)
 	}
 	//Tokenリストから重複のない単語リスト作成
 	
-	def base(Stream:Stream[Token]):Stream[String] =
+    def base(Stream:Stream[Token]): Stream[String] =
         Stream.map((x:Token)=>x.getBaseForm)
 	//Tokenのリストから基の単語リストを取得
 	
-	def reading(Stream:Stream[Token]):Stream[String] = 
+    def reading(Stream:Stream[Token]): Stream[String] = 
         Stream.map((x:Token)=>x.getReading)
 	//Tokenのリストから読みの単語リストを取得
 	
-	def resolverF(path:String):Stream[String] =
+    def resolverF(path:String):Stream[String] =
         mkTokenF(path).map((x:Token)=>x.getBaseForm)
 	//Fileのパスから元の単語リストを取得
 	
-	def count(word:String)(tokens:Stream[Token]):Double = {
-	  tokens match{
-	    case Stream.Empty => 0
-	    case Stream(x) => if(x.getBaseForm==word) 1 else 0
-	    case x#::xs =>
-	      if(x.getBaseForm==word) 1+count(word)(tokens.tail)
-	      else count(word)(tokens.tail)
-	  }
-	 }
+    def count(word:String)(tokens:Stream[Token]):Double = {
+        tokens match{
+            case Stream.Empty => 0
+            case Stream(x) => if(x.getBaseForm==word) 1 else 0
+            case x#::xs =>
+                if(x.getBaseForm==word) 1+count(word)(tokens.tail)
+                else count(word)(tokens.tail)
+                }
+       }
 	//Tokenリストの中に含まれるかもしれない特定の単語の出現回数をカウント
 	
-	def sortCount(path:String):Stream[String] = {
-			mkTokenF(path)
-                .groupBy(x=>x.getBaseForm)
-                .values
-                .toStream.sortWith(_.length>_.length)
-                .map(x=>"count: " +x.length+" "+x(0).getBaseForm)
+    def sortCount(path:String):Stream[String] = {
+        mkTokenF(path)
+            .groupBy(x=>x.getBaseForm)
+            .values
+            .toStream.sortWith(_.length>_.length)
+            .map(x=>"count: " +x.length+" "+x(0).getBaseForm)
 	}
 	//File pathからTokenリストを作成し出現頻度でソート
 
@@ -65,21 +65,21 @@ object  JP{
 //---------------------------------------------------カリー化や合成の試し
 	
 	
-	def condition(cond:Token=>Boolean)=
-	  sortCount("/Users/kyu/Documents/fruits/Exercise/src/textmining/shayo.txt")
+    def condition(cond:Token=>Boolean) = 
+        sortCount("/Users/kyu/Documents/fruits/Exercise/src/textmining/shayo.txt")
 	
-	def oppcond(limit1:String)(limit2:String) =
+    def oppcond(limit1:String)(limit2:String) =
         condition( (x:Token) =>
             (!(x.getPartOfSpeech.startsWith(limit1)) 
             && 
-             !(x.getPartOfSpeech().startsWith(limit2)))
+            !(x.getPartOfSpeech().startsWith(limit2)))
             )
 	
 //	oppcond("助詞")("動詞")
 	
 //	println("")
 	
-	def removenoun(limit:String)=oppcond(limit)("名詞")
+    def removenoun(limit:String) = oppcond(limit)("名詞")
 //	println("")
 //	println("f start")
 //	val does=removenoun("動詞")
