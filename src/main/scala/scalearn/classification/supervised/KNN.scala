72bd27733c0124e4a8e2494091e34d57514bcdef
kyu999 <kyukokkyou999@gmail.com>
2014-06-26 15:05:18 +0900

    improve KNN

--- src/main/scala/scalearn/classification/supervised/KNN.scala
+++ src/main/scala/scalearn/classification/supervised/KNN.scala
@@ -6,27 +6,23 @@ case class KNN(trainData: Vector[(String , Vector[Double])] ){
 
     type className = String
         
-    def classify(subject: Vector[Double], numberOfNeighbors: Int)
-        : (Int , className) = {
-                 
-        val neighbors = 
+    def getNeighbors(subject: Vector[Double], numberOfNeighbors: Int) = 
           trainData
             .map{ data => (data._1 , Stats.euclidean(subject, data._2)) } 
+            .toVector
             .sortBy( class_vector => class_vector._2 ) 
-            .reverse
-            .take(numberOfNeighbors)
-                     
-        println("neighbors : "+neighbors)
-                     
-        val mostMajority = 
-          neighbors    
+            .takeRight(numberOfNeighbors)
             .groupBy(x=>x._1)
             .map(elt=>(elt._2.length,elt._1))
-            .max
-                     
-        mostMajority
         
-    }
+        
+    def classify(subject: Vector[Double], numberOfNeighbors: Int)
+        : (Int , className) 
+    
+    = getNeighbors(subject, numberOfNeighbors).max
+                             
+
+            
     
     /*K-NN法；すべての学習データと手元のデータとの距離をそれぞれもとめ、
       　　　　 距離が近い上位ｋ個の学習データを取り出し、その中で多数派であるクラスを
